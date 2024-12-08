651286621ce9c85a78da7d885a4ba828124a68d4
Eden <you8inpark@gmail.com>
2024-12-09 00:06:09 +0900

    git init

---
+++ phonebook/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  phonebook
+//
+//  Created by eden on 12/8/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+}
+
+
---
+++ phonebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phonebook
+//
+//  Created by eden on 12/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("123")
+    }
+
+
+}
+

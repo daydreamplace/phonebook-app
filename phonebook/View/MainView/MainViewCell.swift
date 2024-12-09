fad4002d046430bc28b99ef9edc3f13fd29148a9
Eden <you8inpark@gmail.com>
2024-12-09 19:58:12 +0900

    feat: 이미지 뷰 생성 및 레이아웃 지정

--- phonebook/View/MainView/MainViewCell.swift
+++ phonebook/View/MainView/MainViewCell.swift
@@ -11,18 +11,36 @@ import SnapKit
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    // MARK: - UI Components
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 25
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemGray6
+        return imageView
+    }()
+    
     // MARK: - Initializers
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Setup Methods
     private func setupUI() {
+        contentView.addSubview(profileImageView)
+        
+        profileImageView.snp.makeConstraints { make in
+            make.size.equalTo(50)
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
         
     }
 }

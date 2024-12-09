f7c11a55247bc3195c5877cd505d0dba9c8ce8ad
Eden <you8inpark@gmail.com>
2024-12-09 11:58:13 +0900

    feat: 버튼 레이블 생성 및 레이아웃 지정

--- phonebook/View/MainView/MainViewController.swift
+++ phonebook/View/MainView/MainViewController.swift
@@ -17,6 +17,14 @@ class MainViewController: UIViewController {
         return label
     }()
     
+    private let addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("추가", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -25,10 +33,16 @@ class MainViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(titleLabel)
+        view.addSubview(addButton)
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(0)
             make.centerX.equalToSuperview()
+        }
+        
+        addButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLabel)
+            make.trailing.equalToSuperview().offset(-16)
         }
     }
 }

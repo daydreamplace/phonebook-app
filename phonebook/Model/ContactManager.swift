7aabb812fb1537416f679049b4e3520d07a083b4
Eden <you8inpark@gmail.com>
2024-12-12 09:26:20 +0900

    feat: 이름, 전화번호 유효성 검사

--- phonebook/Model/ContactManager.swift
+++ phonebook/Model/ContactManager.swift
@@ -13,6 +13,11 @@ class ContactManager {
     private let userDefaultKey = "contacts"
     
     func saveContacts(name: String, phone: String, image: Data?) {
+        guard !name.isEmpty, !phone.isEmpty else {
+            print("이름 또는 전화번호가 비어있습니다.")
+            return
+        }
+        
         var contacts = fetchContacts()
         let newContact = Contact(name: name, phoneNumber: phone, profileImageData: image)
         
